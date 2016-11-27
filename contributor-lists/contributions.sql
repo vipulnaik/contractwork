abbe488d0200d7ff3f87ed089e9e143b67edd110
Vipul Naik <vipulnaik1@gmail.com>
2016-11-27 03:46:22 +0000

    Spelling

--- contributor-lists/contributions.sql
+++ contributor-lists/contributions.sql
@@ -3,7 +3,7 @@ create database contractwork;
 use contractwork;
 
 create table tasks(
-  task_id not null auto_increment,
+  task_id int(11) not null auto_increment,
   worker varchar(40),
   task_venue enum('Wikipedia','Wikipedia Views','Effective Altruism Forum','LessWrong','Personal website') default 'Wikipedia',
   task_type enum('Wikipedia page creation','Wikipedia page update','Wikipedia pge translation','attemped Wikipedia work','miscellaneous Wikipedia work','Preliminary research','Blog post or article','Survey creation','Surey recruitment','Coding','Consulting','Review'),
@@ -13,12 +13,13 @@ create table tasks(
   payment float(7,2) NOT NULL,
   topic varchar(100) NOT NULL,
   format varchar(100) NOT NULL,
-  notes varchar(2000) DEFAULT NULL
+  notes varchar(2000) DEFAULT NULL,
+  primary key (`task_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 # -- Ethan Bashkansky's Wikipedia work
 
-insert into tasks(worker, task_venue, task_type, task_receptacle, task_receptable_url, completion_date, payment, topic, format) values
+insert into tasks(worker, task_venue, task_type, task_receptacle, task_receptacle_url, completion_date, payment, topic, format) values
   ('Ethan Bashkansky', 'Wikipedia', 'Wikipedia page creation','Form W-4','https://en.wikipedia.org/wiki/Form_W-4','2016-02-12',30,'United States taxation','Prose'),
   ('Ethan Bashkansky', 'Wikipedia', 'Wikipedia page creation','Form 1095','https://en.wikipedia.org/wiki/Form_1095','2016-02-13',30,'United States taxation','Prose'),
   ('Ethan Bashkansky', 'Wikipedia', 'Wikipedia page creation','Individual shared responsibility provision','https://en.wikipedia.org/wiki/Individual_shared_responsibility_provision','2016-02-14',40,'United States taxation','Prose'),
