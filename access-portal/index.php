f60e5b5efe41ad13a4c94037a73c45d2e158d81d
Vipul Naik <vipulnaik1@gmail.com>
2016-12-10 08:13:06 +0000

    Started index.php

---
+++ access-portal/index.php
@@ -0,0 +1,6 @@
+<?php
+print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
+print '<title>Contract work for Vipul Naik</title>';
+print '</head>';
+include_once("globalvariables/passwordfile.inc");
+?>
