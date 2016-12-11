8c0377ada2cab1d11456d0fe00f7f5c311ca261d
Vipul Naik <vipulnaik1@gmail.com>
2016-12-11 10:26:21 -0800

    Updated workers file

--- sql/workers.sql
+++ sql/workers.sql
@@ -2,11 +2,11 @@ create table workers(
   worker_id int(11) not null auto_increment primary key,
   worker varchar(40) not null,
   country varchar(20) not null,
-  wikipedia_username varchar(20),
-  facebook_username varchar(20),
+  wikipedia_username varchar(40),
+  facebook_username varchar(100),
   email_id varchar(40),
   preferred_payment_method enum('Cash','Check','PayPal','Wire transfer','Western Union') not null,
-  recruiter enum('Vipul Naik','Issa Rice','Ethan Bashkansky','Linchuan Zhang','Alex K. Chen') not null,
+  recruiter enum('Vipul Naik','Issa Rice','Ethan Bashkansky','Linchuan Zhang','Alex K. Chen','Sebastian Sanchez') not null,
   contact_method enum('inbound email','inbound Facebook','outbound email','outbound Facebook','outbound in-person') not null,
   notes varchar(2000) default null,
   private boolean default false,
@@ -14,6 +14,7 @@ create table workers(
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 insert into workers(worker, country, wikipedia_username, facebook_username, email_id, preferred_payment_method, recruiter, contact_method, notes) values 
+  ('Milo King','United States','MiloJBKing','milo.king','milo.j.king@gmail.com','PayPal','Vipul Naik','outbound Facebook','Did a bit of Wikipedia work in March and then a lot of work on the Market subwiki in the summer of 2016'),
   ('Issa Rice','United States','Riceissa','riceissa','riceissa@gmail.com','Check','Vipul Naik','outbound Facebook','Been working sporadically since 2015; started working regularly since early 2016. Recruited other workers and also helps with managing workers and sprints.'),
   ('Sebastian Sanchez','Argentina','Wikisanchez','seba.sanchez.3998','sebastian123sanchez@gmail.com','Western Union','Vipul Naik','inbound Facebook','Started work in April 2016. Took break in June 2016 for Japan visit. Worked on disease timelines, country healthcare timelines, and miscellaneous global health Wikipedia articles.'),
   ('Ethan Bashkansky','United States','Ethanbas','ethan.bashkansky','ethanbas@gmail.com','PayPal','Issa Rice','outbound in-person','Did sporadic work in February, and has done sporadic work a few times since. Also recruits other workers and stays in touch.'),
@@ -29,4 +30,5 @@ insert into workers(worker, country, wikipedia_username, facebook_username, emai
   ('Sergey Yegorov','Russia','Sergie_Marienhof','avereflection','sashspecial@gmail.com','PayPal','Vipul Naik','inbound Facebook','Sporadic work since September 2016.'),
   ('Thomas Foster','Australia','Thomas_James_Foster','thomasjamesfoster96','me@thomasfoster.co','PayPal','Alex K. Chen','outbound Facebook','Work in June 2016. Could not continue as school term started.'),
   ('Alex K. Chen','United States','Simfish','simfish','simfish@gmail.com','PayPal','Vipul Naik','outbound email','Started working in April 2015 on a unique contract. Continued in 2016, slowed down in last quarter.'),
-  ('Amy Zhang','United States','Amy.zhang425','amy.zhang.5855594','bluemoonamy@live.com','PayPal','Ethan Bashkansky','outbound Facebook','Did some work in June 2016 on timeline of global health. Discontinued work due to other obligations');
+  ('Amy Zhang','United States','Amy.zhang425','amy.zhang.5855594','bluemoonamy@live.com','PayPal','Ethan Bashkansky','outbound Facebook','Did some work in June 2016 on timeline of global health. Discontinued work due to other obligations'),
+  ('Daniel Alejandro Maidana','Argentina','DanielMB','danielalejandro.maidana.50','unknown','PayPal','Sebastian Sanchez','inbound Facebook','Started working in August but delays on both sides so first payment in December');
