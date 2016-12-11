cd9f078a73754e2468fc99e1103d7f8ff64ab969
Vipul Naik <vipulnaik1@gmail.com>
2016-12-11 07:41:29 +0000

    Added task payment by topic file

--- access-portal/index.php
+++ access-portal/index.php
@@ -2,7 +2,7 @@
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 print '<title>Contract work for Vipul Naik</title>';
 print '</head>';
-include_once("globalvariables/passwordfile.inc");
+include_once("backend/globalvariables/passwordfile.inc");
 print '<body>';
 print '<h3>Contract work for Vipul Naik</h3>';
 print '<p>This is an online portal with a summary and various exploration options for contract work done for <a href="http://vipulnaik.com">Vipul Naik</a>. The git repository with the code for this portal, as well as all the underlying data, is <a href="https://github.com/vipulnaik/contractwork">available on GitHub</a>. All payment amounts are in current United States dollars (USD).</p>';
@@ -14,6 +14,7 @@ $monthList = array("2015-04","2015-05","2015-06","2015-07","2015-08","2015-09","
 include ("backend/paymentsDueByTypeAndYear.inc");
 include ("backend/taskPaymentsDueByTypeAndYear.inc");
 include ("backend/taskPaymentsDueByVenueAndYear.inc");
+include ("backend/taskPaymentsDueByTopicAndYear.inc");
 include ("backend/paymentsDueByTypeAndMonth.inc");
 print '</body>';
 ?>
