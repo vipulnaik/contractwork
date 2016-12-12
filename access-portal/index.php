a8a6d62f1229785ea1dd9c1ed30d235628bb4b25
Vipul Naik <vipulnaik1@gmail.com>
2016-12-12 02:08:40 +0000

    More updates to task payments

--- access-portal/index.php
+++ access-portal/index.php
@@ -2,14 +2,11 @@
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 print '<title>Contract work for Vipul Naik</title>';
 print '</head>';
-include_once("backend/globalvariables/passwordfile.inc");
+include_once("backend/globalvariables/passwordFile.inc");
+include_once("backend/globalvariables/lists.inc");
 print '<body>';
 print '<h3>Contract work for Vipul Naik</h3>';
 print '<p>This is an online portal with a summary and various exploration options for contract work done for <a href="http://vipulnaik.com">Vipul Naik</a>. The git repository with the code for this portal, as well as all the underlying data, is <a href="https://github.com/vipulnaik/contractwork">available on GitHub</a>. All payment amounts are in current United States dollars (USD).</p>';
-
-$yearList = array("2015", "2016");
-
-$monthList = array("2015-04","2015-05","2015-06","2015-07","2015-08","2015-09","2015-10","2015-11","2015-12","2016-01","2016-02","2016-03","2016-04","2016-05","2016-06","2016-07","2016-08","2016-09","2016-10","2016-11","2016-12");
 
 include ("backend/paymentsDueByTypeAndYear.inc");
 include ("backend/taskPaymentsDueByTypeAndYear.inc");
--- access-portal/worker.php
+++ access-portal/worker.php
@@ -1,6 +1,7 @@
 <?php
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
-include_once("backend/globalvariables/passwordfile.inc");
+include_once("backend/globalvariables/passwordFile.inc");
+include_once("backend/globalvariables/lists.inc");
 $worker = 'Issa Rice';
 if (!empty($_REQUEST['worker'])) {
   $worker = $_REQUEST['worker'];
@@ -14,8 +15,9 @@ $workerSelectResult = $mysqli -> query($workerSelectQuery);
 if ($workerSelectResult -> num_rows == 0) {
   print '<p>Sorry, we did not find a worker with this name in our public list of workers. Go back to the <a href="/">home page</a> for a full list of workers.</p>';
 } else {
+  print "All payment amounts are listed in current United States dollars (USD).";
   include("backend/workerInfo.inc");
-  include("backend/workerPaymentsDueByType.inc");
+  include("backend/workerPaymentsDueByTypeAndYear.inc");
   include("backend/workerTaskList.inc");
 }
 print '</body>';
