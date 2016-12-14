3cdd8eb15caebaf4f688f846da95448772c35439
Vipul Naik <vipulnaik1@gmail.com>
2016-12-14 09:46:03 -0800

    Updated worker info to include worker file as available on GitHub

--- sql/workers.sql
+++ sql/workers.sql
@@ -18,35 +18,61 @@ create table workers(
   patreon_username varchar(40),
   predictionbook_username varchar(40),
   github_username varchar(40),
+  worker_file varchar(100),
   unique key worker(`worker`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into workers(worker, country, wikipedia_username, facebook_username, email_id, preferred_payment_method, recruiter, contact_method, notes) values 
-  ('Milo King','United States','MiloJBKing','milo.king','milo.j.king@gmail.com','PayPal','Vipul Naik','outbound Facebook','Did a bit of Wikipedia work in March and then a lot of work on the Market subwiki in the summer of 2016'),
-  ('Issa Rice','United States','Riceissa','riceissa','riceissa@gmail.com','Check','Vipul Naik','outbound Facebook','Been working sporadically since 2015; started working regularly since early 2016. Recruited other workers and also helps with managing workers and sprints.'),
-  ('Sebastian Sanchez','Argentina','Wikisanchez','seba.sanchez.3998','sebastian123sanchez@gmail.com','Western Union','Vipul Naik','inbound Facebook','Started work in April 2016. Took break in June 2016 for Japan visit. Worked on disease timelines, country healthcare timelines, and miscellaneous global health Wikipedia articles.'),
-  ('Ethan Bashkansky','United States','Ethanbas','ethan.bashkansky','ethanbas@gmail.com','PayPal','Issa Rice','outbound in-person','Did sporadic work in February, and has done sporadic work a few times since. Also recruits other workers and stays in touch.'),
-  ('Adrian Ulloa','Peru','Physikite','physikite','physikite@outlook.com','PayPal','Vipul Naik','inbound email','Did sporadic work in September of 2016.'),
-  ('Avi Glozman','United States','Avigl','avi.glozman','avi.glozman@gmail.com','PayPal','Ethan Bashkansky','outbound Facebook','Sporadic work since late May/early June 2016'),
-  ('Charlie Corner','United States','Churrupy','charlie.corner.5','churrupy@gmail.com','PayPal','Linchuan Zhang','inbound Facebook','Sporadic work since late June/early July 2016'),
-  ('Christopher Kunin','United States','ChristopherLKunin','chris.kunin.5','Chris.kunin@me.com','PayPal','Ethan Bashkansky','outbound Facebook','Sporadic wok since June 2016'),
-  ('Devansh Dwivedy','United States','Dave8033','devansh33','devarshi@yahoo.com','PayPal','Ethan Bashkansky','outbound Facebook','Sporadic work since June 2016. Note that PayPal belongs to his father'),
-  ('Eddy Su','United States','Esu44224','EddySu98008','esu44224@gmail.com','PayPal','Ethan Bashkansky','outbound Facebook','Made two Wikipedia articles in June 2016.'),
-  ('Jesse Clifton','United States','JesseClifton','jesse.clifton2','jclifton333@gmail.com','Check','Vipul Naik','inbound Facebook','Introduced by Jacy Reese; Jesse had done a project for Jacy at Animal Charity Evaluators and had a summer off'),
-  ('Michael Royzen','United States','Rushingcreek','michael.royzen','michael@mlabtechnologies.com','PayPal','Ethan Bashkansky','outbound Facebook','Wrote one article in June 2016 but could not work further as he wanted to spend more time on developing his app.'),
-  ('Sean Yu','United States','Majesticfish','sean.yu.18','yusean1792@gmail.com','PayPal','Ethan Bashkansky','outbound Facebook','A little work on tax forms in July 2016.'),
-  ('Sergey Yegorov','Russia','Sergie_Marienhof','avereflection','sashspecial@gmail.com','PayPal','Vipul Naik','inbound Facebook','Sporadic work since September 2016.'),
-  ('Thomas Foster','Australia','Thomas_James_Foster','thomasjamesfoster96','me@thomasfoster.co','PayPal','Alex K. Chen','outbound Facebook','Work in June 2016. Could not continue as school term started.'),
-  ('Alex K. Chen','United States','Simfish','simfish','simfish@gmail.com','PayPal','Vipul Naik','outbound email','Started working in April 2015 on a unique contract. Continued in 2016, slowed down in last quarter.'),
-  ('Amy Zhang','United States','Amy.zhang425','amy.zhang.5855594','bluemoonamy@live.com','PayPal','Ethan Bashkansky','outbound Facebook','Did some work in June 2016 on timeline of global health. Discontinued work due to other obligations'),
-  ('Daniel Alejandro Maidana','Argentina','DanielMB','danielalejandro.maidana.50','unknown','PayPal','Sebastian Sanchez','inbound Facebook','Started working in August but delays on both sides so first payment in December'),
-  ('Linchuan Zhang','United States','N/A','linchuan.zhang','unknown','PayPal','Vipul Naik','inbound Facebook','Not a worker, but helped recruit some workers');
+insert into workers(worker, country, wikipedia_username, facebook_username, email_id, preferred_payment_method, recruiter, contact_method, notes, worker_file) values 
+  ('Milo King','United States','MiloJBKing','milo.king','milo.j.king@gmail.com','PayPal','Vipul Naik','outbound Facebook','Did a bit of Wikipedia work in March and then a lot of work on the Market subwiki in the summer of 2016','milo-list.mediawiki'),
+  ('Issa Rice','United States','Riceissa','riceissa','riceissa@gmail.com','Check','Vipul Naik','outbound Facebook','Been working sporadically since 2015; started working regularly since early 2016. Recruited other workers and also helps with managing workers and sprints.', 'issa-list.mediawiki'),
+  ('Sebastian Sanchez','Argentina','Wikisanchez','seba.sanchez.3998','sebastian123sanchez@gmail.com','Western Union','Vipul Naik','inbound Facebook','Started work in April 2016. Took break in June 2016 for Japan visit. Worked on disease timelines, country healthcare timelines, and miscellaneous global health Wikipedia articles.','sebastian-sanchez-list.mediawiki'),
+  ('Ethan Bashkansky','United States','Ethanbas','ethan.bashkansky','ethanbas@gmail.com','PayPal','Issa Rice','outbound in-person','Did sporadic work in February, and has done sporadic work a few times since. Also recruits other workers and stays in touch.','ethan-list.mediawiki'),
+  ('Adrian Ulloa','Peru','Physikite','physikite','physikite@outlook.com','PayPal','Vipul Naik','inbound email','Did sporadic work in September of 2016.','adrian-list.mediawiki'),
+  ('Avi Glozman','United States','Avigl','avi.glozman','avi.glozman@gmail.com','PayPal','Ethan Bashkansky','outbound Facebook','Sporadic work since late May/early June 2016','avi-list.mediawiki'),
+  ('Charlie Corner','United States','Churrupy','charlie.corner.5','churrupy@gmail.com','PayPal','Linchuan Zhang','inbound Facebook','Sporadic work since late June/early July 2016','charlie-list.mediawiki'),
+  ('Christopher Kunin','United States','ChristopherLKunin','chris.kunin.5','Chris.kunin@me.com','PayPal','Ethan Bashkansky','outbound Facebook','Sporadic wok since June 2016','chris-list.mediawiki'),
+  ('Devansh Dwivedy','United States','Dave8033','devansh33','devarshi@yahoo.com','PayPal','Ethan Bashkansky','outbound Facebook','Sporadic work since June 2016. Note that PayPal belongs to his father','devansh-list.mediawiki'),
+  ('Eddy Su','United States','Esu44224','EddySu98008','esu44224@gmail.com','PayPal','Ethan Bashkansky','outbound Facebook','Made two Wikipedia articles in June 2016.','eddy-list.mediawiki'),
+  ('Jesse Clifton','United States','JesseClifton','jesse.clifton2','jclifton333@gmail.com','Check','Vipul Naik','inbound Facebook','Introduced by Jacy Reese; Jesse had done a project for Jacy at Animal Charity Evaluators and had a summer off','jesse-list.mediawiki'),
+  ('Michael Royzen','United States','Rushingcreek','michael.royzen','michael@mlabtechnologies.com','PayPal','Ethan Bashkansky','outbound Facebook','Wrote one article in June 2016 but could not work further as he wanted to spend more time on developing his app.','michael-list.mediawiki'),
+  ('Sean Yu','United States','Majesticfish','sean.yu.18','yusean1792@gmail.com','PayPal','Ethan Bashkansky','outbound Facebook','A little work on tax forms in July 2016.','sean-list.mediawiki'),
+  ('Sergey Yegorov','Russia','Sergie_Marienhof','avereflection','sashspecial@gmail.com','PayPal','Vipul Naik','inbound Facebook','Sporadic work since September 2016.','sergey-list.mediawiki'),
+  ('Thomas Foster','Australia','Thomas_James_Foster','thomasjamesfoster96','me@thomasfoster.co','PayPal','Alex K. Chen','outbound Facebook','Work in June 2016. Could not continue as school term started.','thomas-list.mediawiki'),
+  ('Alex K. Chen','United States','Simfish','simfish','simfish@gmail.com','PayPal','Vipul Naik','outbound email','Started working in April 2015 on a unique contract. Continued in 2016, slowed down in last quarter.','akc-list.mediawiki'),
+  ('Amy Zhang','United States','Amy.zhang425','amy.zhang.5855594','bluemoonamy@live.com','PayPal','Ethan Bashkansky','outbound Facebook','Did some work in June 2016 on timeline of global health. Discontinued work due to other obligations','amy-list.mediawiki'),
+  ('Daniel Alejandro Maidana','Argentina','DanielMB','danielalejandro.maidana.50','unknown','PayPal','Sebastian Sanchez','inbound Facebook','Started working in August but delays on both sides so first payment in December','daniel-list.mediawiki'),
+  ('Linchuan Zhang','United States','N/A','linchuan.zhang','unknown','PayPal','Vipul Naik','inbound Facebook','Not a worker, but helped recruit some workers','linchuan-list.mediawiki');
 
 # -- website lists
 update workers set website='https://issarice.com' where worker = 'Issa Rice';
 
-#-- LessWrong username lists
+#-- LessWrong and EAF username lists
 update workers set lesswrong_username='riceissa' where worker = 'Issa Rice';
 update workers set eaf_username='riceissa' where worker = 'Issa Rice';
 update workers set gratipay_username='riceissa' where worker = 'Issa Rice';
 update workers set eaf_username='JesseClifton' where worker = 'Jesse Clifton';
+
+# -- GitHub username lists
+update workers set github_username='riceissa' where worker = 'Issa Rice';
+
+# -- Worker file insertions
+update workers set worker_file = 'milo-list.mediawiki' where worker = 'Milo King';
+update workers set worker_file = 'issa-list.mediawiki' where worker = 'Issa Rice';
+update workers set worker_file = 'sebastian-sanchez-list.mediawiki' where worker = 'Sebastian Sanchez';
+update workers set worker_file = 'ethan-list.mediawiki' where worker = 'Ethan Bashkansky';
+update workers set worker_file = 'adrian-list.mediawiki' where worker = 'Adrian Ulloa';
+update workers set worker_file = 'avi-list.mediawiki' where worker = 'Avi Glozman';
+update workers set worker_file = 'charlie-list.mediawiki' where worker = 'Charlie Corner';
+update workers set worker_file = 'chris-list.mediawiki' where worker = 'Christopher Kunin';
+update workers set worker_file = 'devansh-list.mediawiki' where worker = 'Devansh Dwivedy';
+update workers set worker_file = 'eddy-list.mediawiki' where worker = 'Eddy Su';
+update workers set worker_file = 'jesse-list.mediawiki' where worker = 'Jesse Clifton';
+update workers set worker_file = 'michael-list.mediawiki' where worker = 'Michael Royzen';
+update workers set worker_file = 'sean-list.mediawiki' where worker = 'Sean Yu';
+update workers set worker_file = 'sergey-list.mediawiki' where worker = 'Sergey Yegorov';
+update workers set worker_file = 'thomas-list.mediawiki' where worker = 'Thomas Foster';
+update workers set worker_file = 'akc-list.mediawiki' where worker = 'Alex K. Chen';
+update workers set worker_file = 'amy-list.mediawiki' where worker = 'Amy Zhang';
+update workers set worker_file = 'daniel-list.mediawiki' where worker = 'Daniel Alejandro Maidana';
+update workers set worker_file = 'linchuan-list.mediawiki' where worker = 'Linchuan Zhang';
+
