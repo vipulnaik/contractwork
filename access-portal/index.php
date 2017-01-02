80003790e722fa97c0c09d71348041f8ee8171a5
Vipul Naik <vipulnaik1@gmail.com>
2017-01-02 05:30:43 +0000

    Added Google Analytics code

--- access-portal/index.php
+++ access-portal/index.php
@@ -1,6 +1,7 @@
 <?php
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 print '<title>Contract work for Vipul Naik</title>';
+include_once('analytics.inc');
 print '</head>';
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/lists.inc");
--- access-portal/topic.php
+++ access-portal/topic.php
@@ -2,7 +2,7 @@
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/lists.inc");
-$worker = 'Issa Rice';
+include_once('analytics.inc');
 $topic = '';
 if (!empty($_REQUEST['topic'])) {
   $topic = $_REQUEST['topic'];
