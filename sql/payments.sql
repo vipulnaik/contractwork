cd3fd15011d3109e4f6fb8cf2844c5872c15fad5
Vipul Naik <vipulnaik1@gmail.com>
2017-01-05 08:43:42 -0800

    Updated Issa payments to mark completion of deposit of 4000 dollar check

--- sql/payments.sql
+++ sql/payments.sql
@@ -60,7 +60,7 @@ insert into payments(payment_method, payee, intermediary, sent_date, payment_not
   ('PayPal','Issa Rice',NULL,'2016-03-04','2016-03-04','2016-03-04','2016-03-04',193,193,0,193),
   ('PayPal','Issa Rice',NULL,'2016-04-22','2016-04-22','2016-04-22','2016-04-22',296,296,0,296),
   ('Check','Issa Rice',NULL,'2016-05-14','2016-05-18','2016-05-23','2016-05-23',4000,4000,0,4000),
-  ('Check','Issa Rice',NULL,'2016-10-01','2016-10-06',NULL,NULL,4000,4000,0,4000),
+  ('Check','Issa Rice',NULL,'2016-10-01','2016-10-06','2017-01-03','2017-01-04',4000,4000,0,4000),
 
   # -- Adrian Ulloa payment
   ('PayPal','Adrian Ulloa','Issa Rice','2016-08-26','2016-08-26','2016-08-26','2016-08-26',105,105.53,0.53,105),
