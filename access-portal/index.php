01594b74c5fb1412e9fac943804718ffbdf04f79
Vipul Naik <vipulnaik1@gmail.com>
2017-01-14 07:35:19 +0000

    Added sortability to one table on homepage, gonna do more now

--- access-portal/index.php
+++ access-portal/index.php
@@ -2,10 +2,19 @@
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 print '<title>Contract work for Vipul Naik</title>';
 include_once('analytics.inc');
+print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
+print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
+print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
 print '</head>';
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/lists.inc");
 print '<body>';
+print '<script>$(document).ready(function()
+    {
+        $("#myTable").tablesorter();
+        $("#myTable2").tablesorter();
+    }
+); </script>'."\n";
 print '<h3>Contract work for Vipul Naik</h3>';
 include_once('preamble.inc');
 print '<h4>Table of contents</h4>';
