9f45c227478672a557de4dfc10b8fe18fefb7f5c
Vipul Naik <vipulnaik1@gmail.com>
2017-02-27 01:38:44 +0000

    Added URL hyperlinking within notes on worker and topic pages

--- access-portal/topic.php
+++ access-portal/topic.php
@@ -2,6 +2,7 @@
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/lists.inc");
+include_once("backend/stringFunctions.inc");
 include_once('analytics.inc');
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
--- access-portal/worker.php
+++ access-portal/worker.php
@@ -2,6 +2,7 @@
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/lists.inc");
+include_once("backend/stringFunctions.inc");
 $worker = 'Issa Rice';
 if (!empty($_REQUEST['worker'])) {
   $worker = $_REQUEST['worker'];
