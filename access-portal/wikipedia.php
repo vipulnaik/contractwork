053b4103a773d01d85fb0f7ea352f75098c8bbfb
Vipul Naik <vipulnaik1@gmail.com>
2017-03-11 18:47:30 +0000

    Added Wikipedia generation script (note that many manual edits are needed to clean up output; this is a one-time use script)

---
+++ access-portal/wikipedia.php
@@ -0,0 +1,62 @@
+<?php
+print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
+include_once("backend/globalVariables/passwordFile.inc");
+include_once("backend/globalVariables/lists.inc");
+include_once("backend/stringFunctions.inc");
+include_once('analytics.inc');
+print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
+print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
+print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
+print "<title>Wikipedia work summary</title></head><body>";
+$pageSelectQuery = "select distinct task_receptacle from tasks where task_type REGEXP 'Wikipedia' and task_type != 'Attempted Wikipedia work' order by task_receptacle;";
+$pageSelectResult = $mysqli -> query($pageSelectQuery);
+
+print '{| class="sortable wikitable" border="1"';
+print "\n! Page !! Creator (if paid) !! Contributors (if paid) !! Connected contributor disclosure added to talk page yet? !! Notes";
+
+function getWikipediaUsername($worker) {
+  global $mysqli;
+  $sqlQuery = "select wikipedia_username from workers where worker='$worker'";
+  $sqlResult = $mysqli -> query($sqlQuery);
+  for ($i = 0; $i < $sqlResult -> num_rows; $i++) {
+    $sqlRow = $sqlResult -> fetch_assoc();
+    return $sqlRow['wikipedia_username'];
+  }
+}
+
+function displayWithLinks($worker) {
+  $wikipediaUsername = getWikipediaUsername($worker);
+  return "[[User:$wikipediaUsername|$wikipediaUsername]] ([[User talk:$wikipediaUsername|talk]], [[Special:Contributions/$wikipediaUsername|contribs]]) ";
+}
+
+for($i = 0; $i < $pageSelectResult -> num_rows; $i++) {
+  $pageRow = $pageSelectResult -> fetch_assoc();
+  $page = $pageRow["task_receptacle"];
+  print "\n|-\n| [[$page]] ";
+  $pageDataQuery = "select * from tasks where task_receptacle=\"$page\" and task_type REGEXP 'Wikipedia' and task_type != 'Attempted Wikipedia work';";
+  $pageDataResult = $mysqli -> query($pageDataQuery);
+  $creator = '';
+  $contributors = array();
+  for($j = 0; $j < $pageDataResult -> num_rows; $j++) {
+    $dataRow = $pageDataResult -> fetch_assoc();
+    $worker = $dataRow['worker'];
+    if ($dataRow['task_type'] == "Wikipedia page creation" or $dataRow['task_type'] == "Wikipedia page translation") {
+      $creator = $worker;
+    } else {
+      array_push($contributors, $worker);
+    }
+  }
+  if ($creator == '') {
+    print " || || ";
+  } else {
+    print " || ".displayWithLinks($creator)." || ";
+  }
+  foreach($contributors as $contributor) {
+    print(displayWithLinks($contributor));
+  }
+  print " || No || ";
+}
+print "\n|}";
+print "</body></html>";
+
+?>
