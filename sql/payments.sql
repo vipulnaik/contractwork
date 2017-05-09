7b9f7334c46a065b43599f62a6c1c49788eea60a
Vipul Naik <vipulnaik1@gmail.com>
2017-05-09 09:38:42 -0700

    Added new Sebastian pay

--- sql/payments.sql
+++ sql/payments.sql
@@ -25,6 +25,7 @@ insert into payments(payment_method, payee, intermediary, sent_date, payment_not
   ('Western Union','Sebastian Sanchez',NULL,'2016-12-21','2016-12-21','2016-12-22','2016-12-22',642.37,642.37,8,634.37),
   ('Western Union','Sebastian Sanchez',NULL,'2017-01-18','2017-01-18','2017-01-19','2017-01-19',640.78,640.78,8,632.78),
   ('Western Union','Sebastian Sanchez',NULL,'2017-03-04','2017-03-04','2017-03-04','2017-03-07',661.62,661.62,8,653.62),
+  ('Western Union','Sebastian Sanchez',NULL,'2017-05-06','2017-05-06','2017-05-06','2017-05-09',659.71,659.71,8,651.71),
   # --Ethan Bashkansky payments
   ('Cash','Ethan Bashkansky','Issa Rice','2016-02-17','2016-02-17','2016-02-17','2016-02-17',115,115,0,115),
   ('PayPal','Ethan Bashkansky',NULL,'2016-04-30','2016-04-30','2016-04-30','2016-04-30',44,44,0,44),
