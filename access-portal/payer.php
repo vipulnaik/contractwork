9824dd71b2d10b293ebefdd77443fb64ac4b5af0
Vipul Naik <vipulnaik1@gmail.com>
2017-09-28 08:50:35 -0700

    Don't show some sections if payer is Vipul Naik

--- access-portal/payer.php
+++ access-portal/payer.php
@@ -16,19 +16,25 @@ print '</head>';
 print '<body>';
 print '<script>$(document).ready(function()
     {
-        $("#myTablePayerPaymentsMadeByMethodAndYear").tablesorter();
+        if ($payer != "Vipul Naik") {
+          $("#myTablePayerPaymentsMadeByMethodAndYear").tablesorter();
+        }
         $("#myTablePayerTaskPaymentsDueByTypeAndYear").tablesorter();
         $("#myTablePayerTaskPaymentsDueByTopicAndYear").tablesorter();
         $("#myTablePayerTaskPaymentsDueByVenueAndYear").tablesorter();
         $("#myTablePayerTaskPaymentsDueByFormatAndYear").tablesorter();
         $("#myTablePayerTaskPaymentsDueByWorkerAndType").tablesorter();
         $("#myTablePayerTaskPaymentsDueByTypeAndMonth").tablesorter();
-        $("#myTablePayerPaymentsDueAndMadeByMonth").tablesorter();
+        if ($payer != "Vipul Naik") {
+          $("#myTablePayerPaymentsDueAndMadeByMonth").tablesorter();
+        }
         $("#myTablePayerTaskPaymentsDueByTopicAndMonth").tablesorter();
         $("#myTablePayerTaskPaymentsDueByVenueAndMonth").tablesorter();
         $("#myTablePayerTaskPaymentsDueByFormatAndMonth").tablesorter();
         $("#myTablePayerTaskList").tablesorter();
-        $("#myTablePayerPaymentList").tablesorter();
+        if ($payer != Vipul Naik") {
+          $("#myTablePayerPaymentList").tablesorter();
+        }
      }
 ); </script>'."\n";
 print "<h3>Contract work sponsored by $payer for Vipul Naik</h3>";
@@ -39,35 +45,47 @@ if ($payerSelectResult -> num_rows == 0) {
 } else {
   print '<h4>Table of contents</h4>';
   print '<ul>';
-  print '<li><a href="#payerPaymentsMadeByMethodAndYear">Payer payments made by method and year</a></li>';
+  if ($payer != "Vipul Naik") {
+    print '<li><a href="#payerPaymentsMadeByMethodAndYear">Payer payments made by method and year</a></li>';
+  }
   print '<li><a href="#payerTaskPaymentsDueByTypeAndYear">Payer task payments due by type and year</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByTopicAndYear">Payer task payments due by topic and year</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByVenueAndYear">Payer task payments due by venue and year</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByFormatAndYear">Payer task payments due by format and year</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByWorkerAndType">Payer task payments due by worker and type</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByTypeAndMonth">Payer task payments due by type and month</a></li>';
-  print '<li><a href="#payerPaymentsDueAndMadeByMonth">Payer task payments due by type and month</a></li>';
+  if ($payer != "Vipul Naik") {
+    print '<li><a href="#payerPaymentsDueAndMadeByMonth">Payer task payments due by type and month</a></li>';
+  }
   print '<li><a href="#payerTaskPaymentsDueByTopicAndMonth">Payer task payments due by topic and month</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByVenueAndMonth">Payer task payments due by venue and month</a></li>';
   print '<li><a href="#payerTaskPaymentsDueByFormatAndMonth">Payer task payments due by format and month</a></li>';
   print '<li><a href="#payerTaskList">Payer task list</a></li>';
-  print '<li><a href="#payerPaymentList">Payer payment list</a></li>';
+  if ($payer != "Vipul Naik") {
+    print '<li><a href="#payerPaymentList">Payer payment list</a></li>';
+  }
   print '</ul>';
   print "<p>All payment amounts are listed in current United States dollars (USD).</p>";
   $printTables = true;
-  include("backend/payerPaymentsMadeByMethodAndYear.inc");
+  if ($payer != "Vipul Naik") {
+    include("backend/payerPaymentsMadeByMethodAndYear.inc");
+  }
   include("backend/payerTaskPaymentsDueByTypeAndYear.inc");
   include("backend/payerTaskPaymentsDueByTopicAndYear.inc");
   include("backend/payerTaskPaymentsDueByVenueAndYear.inc");
   include("backend/payerTaskPaymentsDueByFormatAndYear.inc");
   include("backend/payerTaskPaymentsDueByWorkerAndType.inc");
   include("backend/payerTaskPaymentsDueByTypeAndMonth.inc");
-  include("backend/payerPaymentsDueAndMadeByMonth.inc");
+  if ($payer != "Vipul Naik") {
+    include("backend/payerPaymentsDueAndMadeByMonth.inc");
+  }
   include("backend/payerTaskPaymentsDueByTopicAndMonth.inc");
   include("backend/payerTaskPaymentsDueByVenueAndMonth.inc");
   include("backend/payerTaskPaymentsDueByFormatAndMonth.inc");
   include("backend/payerTaskList.inc");
-  include("backend/payerPaymentList.inc");
+  if ($payer != "Vipul Naik") {
+    include("backend/payerPaymentList.inc");
+  }
 }
 print '</body>';
 ?>
