e2b4cd369b9ed3e0ad516cd7fcaa710f7b8e8440
Vipul Naik <vipulnaik1@gmail.com>
2017-12-16 17:04:52 -0800

    Add tax adjustment

---
+++ sql/tax_adjustments.sql
@@ -0,0 +1,11 @@
+create table tax_adjustments(
+  adjustment_id int(11) not null auto_increment primary key, 
+  worker varchar(40),
+  completion_year varchar(4) not null,
+  payment_finalization_year varchar(4) not null,
+  adjustment float(7,2) not null
+ ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert into tax_adjustments(worker, completion_year, payment_finalization_year, adjustment) values
+  ('Issa Rice','2016','2017','30');
+
