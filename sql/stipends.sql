f3bb6638d59b13906ef5ab191d96fccec5061caa
Vipul Naik <vipulnaik1@gmail.com>
2022-03-14 12:08:54 -0700

    Add @riceissa February stipend

--- sql/stipends.sql
+++ sql/stipends.sql
@@ -95,7 +95,8 @@ insert into stipends(payee, payment, stipend_start_date, stipend_end_date, stipe
   ('Issa Rice',43.92,'2021-10-01','2021-10-31','2021-10-31','Rate of $9/hour effective December 2019',4.88,150),
   ('Issa Rice',33.48,'2021-11-01','2021-11-30','2021-11-30','Rate of $9/hour effective December 2019',3.72,150),
   ('Issa Rice',63.90,'2021-12-01','2021-12-31','2021-12-31','Rate of $9/hour effective December 2019',7.10,150),
-  ('Issa Rice',22.95,'2022-01-01','2022-01-31','2022-01-31','Rate of $9/hour effective December 2019',2.55,150);
+  ('Issa Rice',22.95,'2022-01-01','2022-01-31','2022-01-31','Rate of $9/hour effective December 2019',2.55,150),
+  ('Issa Rice',4.77,'2022-02-01','2022-02-28','2022-02-28','Rate of $9/hour effective December 2019',0.53,150);
 
 insert into stipends(payee, payment, stipend_start_date, stipend_end_date, stipend_award_date, notes, hours_logged) values
   ('Sebastian Sanchez',100,'2017-05-01','2017-05-31','2017-05-31',NULL,NULL),
