ae3da22e41b0194a3a38ba4b833224c637785797
Vipul Naik <vipulnaik1@gmail.com>
2022-06-13 23:23:09 -0700

    Add May 2022 payment due to @riceissa; also add a payment finalized June 2022

--- sql/stipends.sql
+++ sql/stipends.sql
@@ -98,7 +98,8 @@ insert into stipends(payee, payment, stipend_start_date, stipend_end_date, stipe
   ('Issa Rice',22.95,'2022-01-01','2022-01-31','2022-01-31','Rate of $9/hour effective December 2019',2.55,150),
   ('Issa Rice',4.77,'2022-02-01','2022-02-28','2022-02-28','Rate of $9/hour effective December 2019',0.53,150),
   ('Issa Rice',38.25,'2022-03-01','2022-03-31','2022-03-31','Rate of $9/hour effective December 2019',4.25,150),
-  ('Issa Rice',43.20,'2022-04-01','2022-04-30','2022-04-30','Rate of $9/hour effective December 2019',4.80,150);
+  ('Issa Rice',43.20,'2022-04-01','2022-04-30','2022-04-30','Rate of $9/hour effective December 2019',4.80,150),
+  ('Issa Rice',47.07,'2022-05-01','2022-05-31','2022-05-31','Rate of $9/hour effective December 2019',5.23,150);
 
 insert into stipends(payee, payment, stipend_start_date, stipend_end_date, stipend_award_date, notes, hours_logged) values
   ('Sebastian Sanchez',100,'2017-05-01','2017-05-31','2017-05-31',NULL,NULL),
