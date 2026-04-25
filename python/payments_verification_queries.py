f44f9aeb4ff13d90b7c1f6c6d71816850d950d80
Vipul Naik <vipulnaik1@gmail.com>
2026-04-25 12:54:46 -0700

    Add an exception to a verification query; this is a followup to #33

--- python/payments_verification_queries.py
+++ python/payments_verification_queries.py
@@ -5,7 +5,7 @@ queries = [
 
     "select * from payments where payment_method = 'Bitcoin' and not ((sent_date = payment_notice_receipt_date) and (payment_notice_receipt_date = cashout_request_date) and (cashout_request_date = receipt_date));",
 
-    "select * from payments where payee in ('Sebastian Sanchez','Gabriel Bobis') and sent_date >= '2020-06-02' and not (payment_method in ('Bitcoin','FTX','Bitcoin Cash'));",
+    "select * from payments where payee in ('Sebastian Sanchez','Gabriel Bobis') and sent_date >= '2020-06-02' and not (payment_method in ('Bitcoin','FTX','Bitcoin Cash') or (sent_date = '2025-06-02' and payment_method = 'Undisclosed'));",
 ]
 
 _connection = connection.connect()
