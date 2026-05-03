2ae1732d79018a4b149d3daddba8efae592731c5
Vipul Naik <vipulnaik1@gmail.com>
2026-05-02 18:56:46 -0700

    Reword a few common phrases for clarity and consistency

--- python/tasks_verification_queries.py
+++ python/tasks_verification_queries.py
@@ -1,7 +1,7 @@
 import shared, connection
 
 queries = [
-    "select * from tasks where worker = 'Sebastian Sanchez' and task_venue = 'Org Watch' and not (notes regexp concat('last commit for the month was on ',completion_date)) and not (notes regexp 'assisting Gabriel');",
+    "select * from tasks where worker = 'Sebastian Sanchez' and task_venue = 'Org Watch' and not (notes regexp concat('last commit of the month was on ',completion_date)) and not (notes regexp 'assisting Gabriel');",
 ]
 
 _connection = connection.connect()
