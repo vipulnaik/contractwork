953d0c1ca0881e7c90679994e6d7e2e1ffbd0e1c
Vipul Naik <vipulnaik1@gmail.com>
2026-05-02 19:02:17 -0700

    Add a date verification query for timelines page creation tasks; fix date in notes for a few rows flagged by the verification query

--- python/tasks_verification_queries.py
+++ python/tasks_verification_queries.py
@@ -2,6 +2,8 @@ import shared, connection
 
 queries = [
     "select * from tasks where worker = 'Sebastian Sanchez' and task_venue = 'Org Watch' and not (notes regexp concat('last commit of the month was on ',completion_date)) and not (notes regexp 'assisting Gabriel');",
+
+    "select * from tasks where worker = 'Sebastian Sanchez' and task_venue = 'Timelines wiki' and task_type = 'Timelines page creation' and not (notes regexp completion_date or notes regexp date_add(completion_date, interval 1 day));",
 ]
 
 _connection = connection.connect()
